a8d4cd3ad0cef907206167fea9eab279fcf50be2
parris3142 <{ID}+{username}@users.noreply.github.com>
2021-12-04 08:05:30 +0000

    Added comment to explain script

--- run-torch.sh
+++ run-torch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+#
+# Launch interactive Pytorch container with local GPU's allocated
+#
+
 docker run --gpus all -it --rm -v local_dir:/home/parris3142/dev/ngc/temp nvcr.io/nvidia/l4t-pytorch:r32.6.1-pth1.9-py3 bash
 
