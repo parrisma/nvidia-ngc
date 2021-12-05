4ae58d1118095ddf6ee426adc9639b436e4d8708
parris3142 <{ID}+{username}@users.noreply.github.com>
2021-12-05 16:52:17 +0800

    Created container for doing simple torch dev

---
+++ docker-jetson-torch-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build -t parrisma/jetson-torch:1.0 -f dockerfile-jetson-torch .
+#docker push parrisma/jetson-torch:1.0
--- run-torch.sh
+++ run-torch.sh
@@ -4,5 +4,7 @@
 # Launch interactive Pytorch container with local GPU's allocated
 #
 
-docker run --gpus all -it --rm -v local_dir:/home/parris3142/dev/ngc/temp nvcr.io/nvidia/l4t-pytorch:r32.6.1-pth1.9-py3 bash
+#docker run --gpus all -it --rm -v local_dir:/home/parris3142/dev/ngc nvcr.io/nvidia/l4t-pytorch:r32.6.1-pth1.9-py3 bash
+
+docker run --gpus all -it --rm -v /home/parris3142/dev:/home/devuser/dev jetson/torch:1.0 bash
 
